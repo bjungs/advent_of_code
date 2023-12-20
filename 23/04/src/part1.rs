4278d4ab028f3c6caa2dff8d8285968a5621faf2
Bernardo J. C. Nunes <bernardo.nunes@imec.be>
2023-12-20 10:55:59 +0100

    23-04-01 scratchcard mod

--- 23/04/src/part1.rs
+++ 23/04/src/part1.rs
@@ -32,50 +32,9 @@
 //
 // Take a seat in the large pile of colorful cards. How many points are they worth in total?
 
-#[derive(Debug)]
-struct ScratchCard {
-    lucky_numbers: Vec<u32>,
-    game_numbers: Vec<u32>,
-}
+use crate::part1::scratchcard::ScratchCard;
 
-impl ScratchCard {
-    fn calculate_points(&self) -> u32 {
-        self.game_numbers
-            .iter()
-            .filter(|&number| self.lucky_numbers.contains(number))
-            .fold(0, |points, _| match points {
-                0 => 1,
-                _ => points * 2,
-            })
-    }
-}
-
-fn number_sequence(input: &str) -> Vec<u32> {
-    input
-        .split_whitespace()
-        .map(|value| {
-            value
-                .parse::<u32>()
-                .expect("Scratchcard numbers should be valid u32")
-        })
-        .collect()
-}
-
-impl From<&str> for ScratchCard {
-    fn from(str: &str) -> Self {
-        let (lucky_numbers_str, game_numbers_str) = str
-            .split_once(" | ")
-            .expect("Scratchcard format should be \"{lucky_numbers} | {game_numbers}\"");
-
-        let lucky_numbers = number_sequence(lucky_numbers_str);
-        let game_numbers = number_sequence(game_numbers_str);
-
-        ScratchCard {
-            lucky_numbers,
-            game_numbers,
-        }
-    }
-}
+mod scratchcard;
 
 pub fn solve(input: &str) -> u32 {
     let scratchcards: Vec<ScratchCard> = input
---
+++ 23/04/src/part1/scratchcard.rs
@@ -0,0 +1,44 @@
+#[derive(Debug)]
+pub struct ScratchCard {
+    lucky_numbers: Vec<u32>,
+    game_numbers: Vec<u32>,
+}
+
+impl ScratchCard {
+    pub fn calculate_points(&self) -> u32 {
+        self.game_numbers
+            .iter()
+            .filter(|&number| self.lucky_numbers.contains(number))
+            .fold(0, |points, _| match points {
+                0 => 1,
+                _ => points * 2,
+            })
+    }
+}
+
+fn number_sequence(input: &str) -> Vec<u32> {
+    input
+        .split_whitespace()
+        .map(|value| {
+            value
+                .parse::<u32>()
+                .expect("Scratchcard numbers should be valid u32")
+        })
+        .collect()
+}
+
+impl From<&str> for ScratchCard {
+    fn from(str: &str) -> Self {
+        let (lucky_numbers_str, game_numbers_str) = str
+            .split_once(" | ")
+            .expect("Scratchcard format should be \"{lucky_numbers} | {game_numbers}\"");
+
+        let lucky_numbers = number_sequence(lucky_numbers_str);
+        let game_numbers = number_sequence(game_numbers_str);
+
+        ScratchCard {
+            lucky_numbers,
+            game_numbers,
+        }
+    }
+}

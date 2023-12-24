a9bcbcd39ff914dbef73de75059b119e099fb3a9
Bernardo J. C. Nunes <nunes.bjc@gmail.com>
2023-12-24 14:31:44 +0100

    23-04-02 refactoring

--- 23/04/src/scratchcard.rs
+++ 23/04/src/scratchcard.rs
@@ -16,31 +16,18 @@ impl ScratchCardCollection {
             .iter()
             .enumerate()
             .fold(
-                Vec::new(),
-                |mut sc_count_vec: Vec<(&ScratchCard, u32)>, (idx, sc)| {
-                    let count = match sc_count_vec.get(idx) {
-                        Some((_, count)) => *count,
-                        None => {
-                            // set initial count
-                            sc_count_vec.push((&sc, 1));
-                            1
-                        }
-                    };
-
-                    // for every winning number, add 1 more of the next SCs for every copy of this SC
+                // starts with one of each
+                self.0.iter().map(|sc| (sc, 1)).collect(),
+                |mut sc_amount_vec: Vec<_>, (idx, _)| {
+                    // add 1 more of {winning_number} next SCs for every copy of the current SC
+                    let (sc, amount) = sc_amount_vec[idx];
                     for winning_number_idx in 0..sc.winning_numbers().iter().count() {
                         let next_sc_idx = idx + winning_number_idx + 1;
-                        match sc_count_vec.get_mut(next_sc_idx) {
-                            Some((_, next_count)) => {
-                                *next_count += count;
-                            }
-                            None => {
-                                sc_count_vec.insert(next_sc_idx, (sc, 1 + count));
-                            }
+                        if let Some((_, next_count)) = sc_amount_vec.get_mut(next_sc_idx) {
+                            *next_count += amount;
                         }
                     }
-
-                    sc_count_vec
+                    sc_amount_vec
                 },
             )
             .iter()

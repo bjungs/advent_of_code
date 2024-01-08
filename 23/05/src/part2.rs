ae0332140c4df94b6c4486b29aea9ca46fb7e974
Bernardo J. C. Nunes <bernardo.nunes@imec.be>
2024-01-08 10:24:24 +0100

    23-05-02 fixed seed ranges digestion

--- 23/05/src/part2.rs
+++ 23/05/src/part2.rs
@@ -2,29 +2,18 @@ use crate::almanac::Almanac;
 use std::ops::Range;
 
 fn get_seed_ranges(seeds_values: Vec<u64>) -> Vec<Range<u64>> {
-    let mut range_start = None;
-    let mut range_length = None;
-
-    let mut ranges = Vec::new();
-    for seed_value in seeds_values {
-        if range_start.is_some() {
-            if range_length.is_some() {
-                let start = range_start
-                    .take()
-                    .expect("We just checked that this value is Some.");
-                let length = range_length
-                    .take()
-                    .expect("We just checked that this value is Some.");
-                ranges.push(start..start + length)
-            } else {
-                range_length = Some(seed_value);
-            }
-        } else {
-            range_start = Some(seed_value);
-        }
-    }
-
-    ranges
+    seeds_values
+        .chunks(2)
+        .map(|chunk| {
+            assert_eq!(
+                chunk.len(),
+                2,
+                "The seed ranges input should always contain numbers in pairs"
+            );
+            let [start, length]: [u64; 2] = chunk.try_into().unwrap();
+            start..(start + length)
+        })
+        .collect::<Vec<_>>()
 }
 
 pub fn solve(input: &str) -> u64 {
